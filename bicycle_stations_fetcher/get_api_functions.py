27ef810e6e9c81875ec91a6a5239d7fb6de09be5
lubba <hyacinthe.chaigne@gmail.com>
2021-09-20 10:34:48 +0200

    get api functions

---
+++ bicycle_stations_fetcher/get_api_functions.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+
+def get_vlille():
+    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet" \
+          "=libelle&facet=commune&facet=etat&facet=type&facet=etatconnexion "
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
+
+
+def get_vlyon_availability():
+    url = "https://transport.data.gouv.fr/gbfs/lyon/station_status.json"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    data = response_json.get("data", [])
+    return data["stations"]
+
+
+def get_vlyon_geo():
+    url = "https://transport.data.gouv.fr/gbfs/lyon/station_information.json"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    data = response_json.get("data", [])
+    return data["stations"]
+
+
+def get_vrennes_geo():
+    url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=topologie-des-stations-le-velo-star&q=&facet=codeinseecommune&facet=nomcommune&facet=coordonnees&facet=nombreemplacementstheorique&facet=possedetpe"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
+
+
+def get_vrennes_availability():
+    url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
+
+
+def get_vparis():
+    url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+    response = requests.request("GET", url)
+    response_json = json.loads(response.text.encode('utf8'))
+    return response_json.get("records", [])
+
