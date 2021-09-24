9026f6decf5042b30f14a6cff1e5fd41005a7e1d
lubba <hyacinthe.chaigne@gmail.com>
2021-09-24 15:59:52 +0200

    unfinished

--- client/client.py
+++ client/client.py
@@ -1,12 +1,18 @@
 from threading import Thread
+from pymongo import MongoClient
+
+from bicycle_stations_fetcher.database_insert import get_database
 from worker.worker import worker
 
 # Attempt start worker to fetch api data every min and populate mongo db
-try:
-    worker = Thread(target=worker)
-    worker.start()
-except:
-    print("Failed to start worker")
+# try:
+#    worker = Thread(target=worker)
+#    worker.start()
+# except:
+#    print("Failed to start worker")
+
+# fetch db once
+db = get_database()
 
 # Client interaction with data
 while True:
@@ -19,4 +25,18 @@ while True:
     print("city is: " + city)
     print("long/lat " + long + "/" + lat)
 
-    # Process on mongo and sho result
+    # Process on mongo and show result
+    db.collection.create_index('coordinates', name="2dsphere")
+    query = db["fr-lille"].find({
+        "coordinates": {
+            "$near": {
+                "$geometry": {
+                    type: "Point",
+                    "coordinates": [long, lat]
+                },
+            }
+        }
+    })
+
+    for element in query:
+        print(element)
--- worker/worker.py
+++ worker/worker.py
@@ -7,28 +7,24 @@ from bicycle_stations_fetcher.get_api_functions import *
 def send_to_mongo(city):
     if city == "Lille":
         print("Fetching Lille...")
-        json = get_vlille()
-        # now send it
+        get_lille()
         print("Successfully fetched and moved to mongo")
 
     elif city == "Paris":
         print("Fetching Paris...")
-        json = get_vparis()
-        # now send it
+        get_paris()
         print("Successfully fetched and moved to mongo")
 
     elif city == "Rennes":
         print("Fetching Rennes...")
-        json_geo = get_vrennes_geo()
-        json_availability = get_vrennes_availability
-        # now send it
+        get_rennes_geo()
+        get_rennes_availability()
         print("Successfully fetched and moved to mongo")
 
     elif city == "Lyon":
         print("Fetching Lyon...")
-        json_geo = get_vlyon_availability
-        json_availability = get_vlyon_geo
-        # now send it
+        get_lyon_availability()
+        get_lyon_geo()
         print("Successfully fetched and moved to mongo")
 
     else:
