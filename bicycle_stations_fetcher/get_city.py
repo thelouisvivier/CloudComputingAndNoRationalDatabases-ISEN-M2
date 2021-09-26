4e7871937b466e5eabd56276b402dd83cb4b1601
lubba <hyacinthe.chaigne@gmail.com>
2021-09-26 19:16:44 +0200

    find + fix client

---
+++ bicycle_stations_fetcher/get_city.py
@@ -0,0 +1,17 @@
+# Get and return city name
+def get_city():
+    city = input("Enter city (Lille/Paris/Rennes/Lyon) : ")
+
+    if city == "Lille":
+        name = "fr-lille"
+    elif city == "Paris":
+        name = "fr-paris"
+    elif city == "Rennes":
+        name = "fr-rennes"
+    elif city == "Lyon":
+        name = "fr-lyon"
+    else:
+        print("Wrong city name! Exciting...")
+        exit(1)
+
+    return name
